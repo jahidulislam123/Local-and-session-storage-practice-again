3f31e768529987cab9a3a9b1a4443fcd8f02ed90
unknown <islam15-3879@diu.edu.bd>
2022-03-28 15:36:38 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const fruits =[
+    {name:"pineapple"},
+    {name:"mango"},
+    {name:"papya"}
+]
+const election =['razzak','alam','jayed','jayed','jayed','nipon','jayed'];
+//eta theke jayed kotobar jevabe ber korbo
+
+
+const election2 ={
+    razzak:1,
+    alomgir:1,
+    jashim:1,
+    jayed:4
+}
+console.log(election2.jayed);
+
+
+
+const db={
+
+}
+
+const addToDb=(item)=>{
+    //3 ways to add a propperty to a object
+    // db.alom=1;system 1
+    // db['alom']=1; second system
+    // db[item]=1;
+
+
+    if(item in db){
+        db[item]=db[item]+1;
+        //bam pasher db[item ]hocce man set korar jonno r dan pasher db[item ]man koto ase seta pawar jonno ebong tar sathe 1 jog korar jonno
+
+    }
+    else{
+        db[item]=1;
+        //tem na thakle item er man 1 bosay dibo 
+    }
+    console.log(item);
+
+}
+
+//local storage e data rakhte gele amake 3 ta kaj korte hobe segula holo
+//1 : local storage.get item 
+//2: local storage .set item 
+//3:set item dite hole 2 ta jinish dite hobe ekta holo key opor ta hoilo value 
+
